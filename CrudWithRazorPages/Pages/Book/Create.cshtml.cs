c0d417563f0f4ba9ab2e6ef84988737472382132
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-12-18 00:48:42 +0200

    - Create page added (Book Folder)
    - The Create Html content added
    
    - The Create.cs html.cs  Added OnPost( )  method to use Dbcontext to add a Book to the Database
    
    - added closing </div> tag to the book's index html page

---
+++ CrudWithRazorPages/Pages/Book/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudWithRazorPages.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CrudWithRazorPages.Pages.Book
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty] // with this DataAnnotation we can use directly the book object as we had OnPost (Book bookObj) 
+        public Model.Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
